b140d5f6cffa38dbc98ea081b85abd30c02b8ca2
dkin12 <dkin1020@naver.com>
2025-11-02 01:47:40 +0900

    [init] springBoot 프로젝트 설정
    프로젝트 설정

---
+++ ChoiceMate/src/main/java/com/example/choicemate/ChoiceMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.choicemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoiceMateApplication.class, args);
+    }
+
+}
---
+++ ChoiceMate/src/test/java/com/example/choicemate/ChoiceMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.choicemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
