49bee49e6904f0af7aa24b845f0042f651d9d481
dkin12 <dkin1020@naver.com>
2025-11-10 18:58:24 +0900

    [add] html 파일 수정 - 모달 팝업 추가 - 부트스트랩 테마 수정 - 투표 html 추가

--- ChoiceMate/src/main/java/com/example/choicemate/controller/PostController.java
+++ ChoiceMate/src/main/java/com/example/choicemate/controller/PostController.java
@@ -27,6 +27,15 @@ public class PostController {
     public String showLatestPost(Model model) {
         PostDto latestPost = service.getLatestPost();
         model.addAttribute("post", latestPost);
+
+        /*
+         TODO 소령
+         여기 true 가 투표 된 것
+         기본 false로 하고, 나중에 투표하면 true로 바꿔서
+         DB에서 개수 불러와서 보여주면됨
+         ps) 선택한거 값 넘겨서 라디오에 표시도 해주면 좋고,,
+        */
+        model.addAttribute("voted", false);
         return "index";
     }
 }
